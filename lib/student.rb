2dc9db842440cda5eb95c923af7980b05c761fe0
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-30 13:51:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,4 +41,12 @@ class Student < InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).ni?
+    end
+    values.join(", ")
+  end
+
 end
