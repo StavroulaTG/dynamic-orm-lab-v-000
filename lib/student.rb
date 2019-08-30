24aba6015e0229c1f5c28579b19582396f79ed1a
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-30 14:06:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,9 @@ class Student < InteractiveRecord
   end
 
   def self.find_by(attribute)
-    
+    column_name = attribute.keys[0].to_sym
+    value_name = attribute.values[0]
+
     sql = <<-SQL
     SELECT * FROM #{table_name}
     WHERE #{column_name} = ?
