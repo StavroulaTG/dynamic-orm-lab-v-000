1081527948f15076e3c1a479b466d9a51cafd647
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-30 13:30:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,7 @@ class Student < InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(",")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
 end
