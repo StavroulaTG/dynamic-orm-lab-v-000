65328bdfc04701bc4a081a6e03ef5138d64fb9d4
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-30 13:22:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,4 +27,8 @@ class Student < InteractiveRecord
     attr_accessor col_name.to_sym
   end
 
-end
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
