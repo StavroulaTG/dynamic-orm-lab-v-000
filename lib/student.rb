a4a4a5254bcd6f8a57da83d09d382d3311f63135
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-30 14:11:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,10 +65,7 @@ class Student < InteractiveRecord
     column_name = attribute.keys[0].to_s
     value_name = attribute.values[0]
 
-    sql = <<-SQL
-    SELECT * FROM #{table_name}
-    WHERE #{column_name} = ?
-    SQL
+
 
     DB[:conn].execute(sql, value_name)
   end
